972d81fcad95c3e47343d954851e447b8e056b34
ufukarmagan <ufukarmaganozgur@gmail.com>
2021-03-29 17:14:45 +0300

    Update TestView.java

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -5,7 +5,10 @@ import java.time.LocalDateTime;
 import java.math.BigDecimal;
 import javax.annotation.PostConstruct;
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.model.LazyDataModel;
 
 import lombok.Data;
 
@@ -19,9 +22,36 @@ public class TestView implements Serializable {
     private BigDecimal decimal;
     private LocalDateTime localDateTime;
     
+    private LazyDataModel<Customer> lazyModel;
+    
+    private CustomerService service = new CustomerService();
+    
     @PostConstruct  
     public void init() {
         string = "Welcome to PrimeFaces!!!";
+        lazyModel = new LazyCustomerDataModel(service.getCustomers());
     }
 
+    public String getString() {
+      return string;
+    }
+
+    public void setString(String string) {
+      this.string = string;
+    }
+
+    public LazyDataModel<Customer> getLazyModel() {
+      return lazyModel;
+    }
+
+    public CustomerService getService() {
+      return service;
+    }
+
+    public void setService(CustomerService service) {
+      this.service = service;
+    }
+
+    
+    
 }
